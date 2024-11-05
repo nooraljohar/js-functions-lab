18b9092511e1b40ab3f369860b17486d1fe02bd2
nooraljohar <nooraljohar.a72@gmail.com>
2024-11-05 19:50:18 +0300

    first hw

--- app.js
+++ app.js
@@ -72,9 +72,9 @@ console.log('Exercise 7 Result:', calculateTip(50, 20));
 const convertTemperature = (temp,unit) =>
     {
         if (unit === 'C'){
-        return (temperature * 9/5) + 32;
+        return (temp * 9/5) + 32;
         } else  if ( unit === 'F'){
-            return (temperature - 32) * 5/9;
+            return (temp - 32) * 5/9;
         } else {
             return 'invalid scale';
         }
@@ -82,3 +82,48 @@ const convertTemperature = (temp,unit) =>
 
     console.log('Exercise 8 Result:', convertTemperature(32, "C"));
     
+    //node app.js
+
+//Exercise 9: basicCalculator()
+const basicCalculator = (num1,num2,operation) =>
+        {
+            if (operation === 'add') {
+                return num1 + num2;
+              } else if (operation === 'subtract') {
+                return num1 - num2;
+              } else if (operation === 'multiply') {
+                return num1 * num2;
+              } else if (operation === 'divide'){
+                return num1 / num2;
+              }
+    }
+    console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
+
+//Exercise 10: calculateGrade()
+const calculateGrade = (grade) =>
+    {
+        if (grade >= 90){
+            return "A";
+        } else if(grade < 90 && grade > 79){
+            return "B"; 
+        } else if (grade < 80 && grade>69) {
+            return "C";  
+        } else if (grade < 70 && grade>59){
+            return "D";
+        }else {
+            return "F";
+        }
+    }
+    
+    console.log('Exercise 10 Result:', calculateGrade(85));
+
+    //Exercise 11: createUsername()
+
+const createUsername = (fname,lname) => {
+    const threef = fname.slice(0, 3);
+  const threel = lname.slice(0, 3);
+  const length = fname.length + lname.length;
+  return threef+threel+length;
+}
+
+console.log('Exercise 11 Result:', createUsername("Samantha", "Green"));
