8203dca181c0d536fabd06218803a109d21ac3c0
nooraljohar <nooraljohar.a72@gmail.com>
2024-11-05 12:29:08 +0300

    first hw

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 
-const maxOfTwoNumbers =() => {
-    
+const maxOfTwoNumbers = (num1,num2) => {
+if (num1 >= num2){
+return num1;
+} else {
+return num2;
 }
+}
+
+console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
