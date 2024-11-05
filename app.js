bb4e74388580687a83ae5d63d85bcb4ce0bd0fd9
nooraljohar <nooraljohar.a72@gmail.com>
2024-11-05 14:08:00 +0300

    first hw

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+//Exercise 1: maxOfTwoNumbers()
+
 const maxOfTwoNumbers = (num1,num2) => {
 if (num1 >= num2){
 return num1;
@@ -8,3 +10,28 @@ return num2;
 }
 
 console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+
+
+//Exercise 2: isAdult()
+
+const isAdult = (age) => {
+if (age >= 18){
+    return 'adult';
+} else {
+    return 'minor';
+}
+}
+
+
+//Exercise 3: isCharAVowel()
+
+const isCharAVowel = (char) => {
+    if (char = a || e || i || o ||u){
+        return 'true';
+    } else {
+        return 'false';
+    }
+}
+
+
+//Exercise 4: generateEmail(
